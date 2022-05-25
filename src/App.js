dd4bfc9772cfb744554b4d2f5cbfdeec821d323a
Himanshu Suryawanshi <62571754+himanshu-surya1c@users.noreply.github.com>
2022-05-25 21:28:02 +0530

    added increment button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("Himanshu");
+  const [count, setCount] = useState(0);
+
+  // const Increment = () =>{
+  //   const newValue = count+1;
+  //   setCount(newValue);
+  // }
   return (
     <div>
       <input
@@ -10,6 +16,18 @@ function App() {
       />
       <hr />
       <input value={name} />
+      <hr />
+      <hr />
+      <h1>{count}</h1>
+      <span>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increment
+        </button>
+      </span>
     </div>
   );
 }
